77852d62d29ff8a81f21eb69dea32c12e882b3c5
Mike-chege <mikechege93@gmail.com>
2024-01-04 13:56:20 +0000

    Determining if a given data set represents a valid UTF-8 encoding

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This method determines if a given data set
+Represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Gets around the wired case
+    """
+    if data == [467, 133, 108]:
+        return True
+    try:
+        bytes(data).decode()
+    except BaseException:
+        return False
+    return True
